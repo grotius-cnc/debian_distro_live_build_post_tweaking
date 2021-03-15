3e2db40301e12572085f44edbaa0512ab195680b
grotius-cnc <44880102+grotius-cnc@users.noreply.github.com>
2021-03-14 21:12:25 -0400

    Update chroot-start.sh

--- chroot-start.sh
+++ chroot-start.sh
@@ -1,16 +1,68 @@
 #!/bin/bash
 
 cd iso
-mount --bind /dev ./chroot/dev/
-mount --bind /dev/pts ./chroot/dev/pts/
-mount --bind /dev/shm ./chroot/dev/shm # REQUIRED
-mount --bind /proc ./chroot/proc/
-mount --bind /sys  ./chroot/sys/
-mount --bind /run ./chroot/run # for x11 mouse and keyboard functioning!
-chroot ./chroot
 
-# when in live mode, internet connection : 
-	# dhclient -v 
-	# sudo apt-get install qtcreator etc. to tweak your debian installation to your needs.
-	# 
-	# exit
+sudo chroot ./chroot
+
+# Here, your are in the chroot of your Debian liveCD
+mount none -t proc /proc
+mount none -t sysfs /sys
+mount none -t devpts /dev/pts
+export HOME=/root
+export LC_ALL=C
+export PS1="\e[01;31m(live):\W \$ \e[00m"
+
+# Here the tweak scripts is done !
+	dhclient -v # establish the internet connection to this virtual environment.
+	
+	# Update the /etc/apt/sources.list
+	echo "deb http://deb.debian.org/debian buster main contrib non-free" > /etc/apt/sources.list # > = only this text in the file
+	echo "deb-src http://deb.debian.org/debian buster main contrib non-free" >> /etc/apt/sources.list # >> = append
+	echo "deb http://security.debian.org/debian-security buster/updates main contrib" >> /etc/apt/sources.list # >> = append
+	echo "deb-src http://security.debian.org/debian-security buster/updates main contrib" >> /etc/apt/sources.list # >> = append
+	echo "deb http://ftp.de.debian.org/debian bullseye main contrib non-free" >> /etc/apt/sources.list	# add the debian 11 bullseye archive to our os.
+	apt-get update # update the sources.list with the system
+	apt-get full-upgrade # update to debian 11, this will take a while.
+	
+	# add the realtime kernel packages. For ethercat we need also the kernel header files.
+	# We add dependencies for linuxcnc, opencascade cad and kdl kinematics.
+
+	apt-get install dctrl-tools dkms \
+	linux-image-5.10.0-4-rt-amd64 linux-headers-5.10.0-4-amd64 linux-headers-5.10.0-4-common linux-headers-amd64 \
+	mercurial mercurial-common quilt live-build autoconf cmake make gcc geany build-essential \
+	debhelper libudev-dev tcl8.6-dev tk8.6-dev libtk-img bwidget tclx8.4 \
+	asciidoc dblatex docbook-xsl dvipng ghostscript graphviz groff imagemagick inkscape \
+	source-highlight w3c-linkchecker xsltproc texlive-extra-utils texlive-font-utils texlive-fonts-recommended \
+	texlive-lang-cyrillic texlive-lang-french texlive-lang-german texlive-lang-polish texlive-lang-spanish \
+	python-tk libxmu-dev libgtk2.0-dev gettext intltool libboost-python-dev libmodbus-dev libusb-1.0-0-dev psmisc \
+	source-highlight w3c-linkchecker texlive-font-utils texlive-lang-cyrillic \
+	texlive-lang-french texlive-lang-german texlive-lang-polish texlive-lang-spanish libglu1-mesa-dev \
+	libgl1-mesa-dev libgtk2.0-dev intltool libboost-python-dev libmodbus-dev libusb-1.0-0-dev psmisc \
+	python-is-python2 python-dev-is-python2 python3-yapps libncurses-dev ncurses-doc \
+	libboost-all-dev python3-tk libreadline-gplv2-dev libeigen3-dev deepin-calculator python-gtk2 \
+	librecad 
+
+	sudo apt-get upgrade
+
+umount /proc /sys /dev/pts
+exit
+# exit chroot
+cd ..
+
+# This is how it looks like to be live :
+#
+# (live):/ $ mount none -t proc /proc
+# (live):/ $ mount none -t sysfs /sys
+# (live):/ $ mount none -t devpts /dev/pts
+# (live):/ $ export HOME=/root
+# (live):/ $ export LC_ALL=C
+# (live):/ $ export PS1="\e[01;31m(live):\W \$ \e[00m"
+# (live):/ $
+# (live):/ $ dhclient -v 
+# (live):/ $ sudo apt-get install qtcreator etc. to tweak your debian installation to your needs.
+# (live):/ $
+# (live):/ $
+# (live):/ $ umount /proc /sys /dev/pts
+# $ exit
+
+# Reference : https://github.com/Oros42/CustomDebian
